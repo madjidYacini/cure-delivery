1b036cb7432df48cba08991b64e4fb084bef1548
madjid yacini <madjid.yacini@gmail.com>
2018-05-30 20:48:57 +0200

    commande V1

---
+++ controllers/commande.js
@@ -0,0 +1,36 @@
+// const medicament = require("../config/medicaments.json");
+import { Commande } from "../models";
+
+exports.store_commande = (req,res)=>{
+    console.log(req.params)
+    let {id,pid,ordid }= req.params;
+    let commandeStatus = req.body.commandeStatus;
+    let totalPrice = req.body.totalPrice;
+    const commande = {
+       UserId: id,
+       PharmacieId: pid,
+       OrdonanceId: ordid,
+        statelivraison:commandeStatus,
+        totalprice:totalPrice
+    }
+
+
+    // insert the command
+    Commande.create(commande)
+    .then((commande) => {
+       if (!commande) {
+           res.status(400).json({
+               message:"sorry the command was not registred"
+           })
+       }else{
+           res.status(200).json({
+               message:"command registered",
+               command : commande
+           })
+       } 
+    }).catch((err) => {
+        console.log(err.message)
+    });
+     
+    // console.log(commande);
+}
