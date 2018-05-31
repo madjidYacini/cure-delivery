3b8eb36e8504be5fc2eda2d1224e7c707b12dac3
madjid yacini <madjid.yacini@gmail.com>
2018-05-31 14:16:44 +0200

    server.js

--- server.js
+++ server.js
@@ -47,15 +47,15 @@ app.use(
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend: true }));
-// app.use((req,res,next)=>{
-//     res.header('Access-Control-Allow-Origin','*');
-//     res.header ('Access-Control-Allow-Headers','Origin , X-Requested-With, Content-Type, Accept, Authorization ');
-//     if(req.method === 'OPTIONS'){
-//         res.header('Access-Control-Allow-Methods','PUT, POST, PATCH , DELETE, GET ');
-//         res.status(200).json({})
-//     }
-//     next(); 
-// })
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header ('Access-Control-Allow-Headers','Origin , X-Requested-With, Content-Type, Accept, Authorization ');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH , DELETE, GET ');
+        res.status(200).json({})
+    }
+    next(); 
+})
 db.sequelize.sync({}).then(() => {
   app.use("/api", routes);
 
